72e434e7f0fccb1a046c12ea719c559649d814e2
rachelqxl <rl2563@nyu.edu>
2019-03-11 15:58:04 -0400

    in-class stuff

---
+++ CodeLabWeek6/Assets/Scripts/AsciiLevelLoader.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+public class AsciiLevelLoader : MonoBehaviour
+{
+    private string filePath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        filePath = Application.dataPath + "/level0.txt";
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath,"X");
+        }
+        
+        string[] inputLines = File.ReadAllLines(filePath);
+        
+        for (int y = 0; y < inputLines.Length; y++)
+        {
+            string line = inputLines[y];
+           for (int x= 0; x <line.Length; x++)
+           {
+            
+//               if (line[x] == 'X')
+//               {
+//                    GameObject newWall = Instantiate(Resources.Load("Wall") as GameObject);
+//                   newWall.transform.position = new Vector2(x-line.Length/2, (inputLines.Length/2 - y));
+//                  
+//               }
+//               if (line[x] == 'P')
+//               {
+//                   GameObject newPlayer = Instantiate(Resources.Load("Player") as GameObject);
+//                   newPlayer.transform.position = new Vector2(x-line.Length/2, (inputLines.Length/2 - y));
+//                  
+//               }
+//               if (line[x] == 'T')
+//               {
+//                   GameObject newTrap = Instantiate(Resources.Load("Trap") as GameObject);
+//                   newTrap.transform.position = new Vector2(x-line.Length/2, (inputLines.Length/2 - y));
+//                  
+//               }
+//               if (line[x] == 'G')
+//               {
+//                   GameObject newGoal = Instantiate(Resources.Load("Goal") as GameObject);
+//                   newGoal.transform.position = new Vector2(x-line.Length/2, (inputLines.Length/2 - y));
+//                  
+//               }
+
+               GameObject tile;
+               switch (line[x])
+               {
+                   case'X':
+                      tile = Instantiate(Resources.Load("Wall") as GameObject);
+                       break;
+                   case'P':
+                       tile =Instantiate(Resources.Load("Player") as GameObject);
+                       break;
+                   case'G':
+                       tile =Instantiate(Resources.Load<GameObject>("Goal") );
+                       break;
+                   case'T':
+                       tile =  Instantiate(Resources.Load<GameObject>("Trap"));
+                       break;
+                   default:
+                       tile = null;
+                       break;
+                       
+               }
+
+               if (tile != null)
+               {
+                   tile.transform.position = new Vector2(x-line.Length/2, (inputLines.Length/2 - y));
+               }
+
+           }
+        }
+        
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
---
+++ CodeLabWeek6/Assets/Scripts/HighScoreManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class HighScoreManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<string> hsNames;
+    public List<int> hsScores;
+    int num = 10;
+    void Start()
+    {
+        string filePath = Application.dataPath + "/highScore.txt";
+        if (!File.Exists(filePath))
+        {
+            string output = "";
+            
+            for (int i = 0; i < num; i++)
+            {
+                output += "Matt:" + (10-i) + '\n';
+            }
+            Debug.Log("output" + output);
+            File.WriteAllText(filePath,output);
+           // output = "Matt:10\nMatt:9"; // \n to create new line 
+        }
+
+        string[] inputLines = File.ReadAllLines(filePath); //read by \n
+        for (int i = 0; i < inputLines.Length; i++)
+        {
+            string line = inputLines[i]; // "Matt:10"
+            string[] splitLine = line.Split(':');// "Matt" | "10"
+            string name = splitLine[0];//"Matt"
+            int score = Int32.Parse(splitLine[1]);
+            hsNames.Add(name); //put name in my list of names
+            hsScores.Add(score);
+            
+
+        }
+
+
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
