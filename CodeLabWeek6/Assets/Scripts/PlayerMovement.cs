735a01f955b2f4ce3727ffbeab57ef30683c5059
rachelqxl <rl2563@nyu.edu>
2019-03-25 13:13:58 -0400

    player movement script

---
+++ CodeLabWeek6/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D myRB;
+    public float speed; 
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRB.velocity = (Vector2.right * Input.GetAxis("Horizontal") + Vector2.up * Input.GetAxis("Vertical")) * speed;
+    }
+}
