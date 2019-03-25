a1e3d932b3c9c2e3a9546428f3de3f8eb2b1d2e1
rachelqxl <rl2563@nyu.edu>
2019-03-25 14:17:10 -0400

    moving trap script

---
+++ CodeLabWeek6/Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    public float velocity;
+    
+    private void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * velocity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        velocity = -velocity;
+    }
+}
---
+++ CodeLabWeek6/Assets/Scripts/ResetGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResetGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D myRB;
+    public Text StateText;
+    
+    private void Start()
+    {
+        StateText = GameObject.Find("Text").GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("r"))
+            SceneManager.LoadScene(0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.gameObject.tag.Equals("Trap"))
+        {
+            StateText.text = "Game Over :(";
+            Destroy(this.gameObject);
+        }
+        
+        if (other.gameObject.tag.Equals("Goal"))
+        {
+            StateText.text = "You Win!";
+            Destroy(other.gameObject);
+            Destroy(GetComponent<PlayerMovement>());
+            Destroy(myRB);
+        }
+        
+    }
+}
